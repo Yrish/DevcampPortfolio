bce42ce25daa72a5c681ae707299da6f1fb0f983
Yrish <Yrish3@gmail.com>
2017-06-21 15:23:20 +0000

    Implemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+3.times do |topic|
+    Topic.create!(title: "An amazing topic ##{topic}")
+end
+
+puts "finished with topics"
+
 10.times do |blog|
    Blog.create!(
     title: "My Blog Post #{blog}",   
-    body: "This is Post #{blog}"
+    body: "This is Post #{blog}",
+    topic_id: (blog % 3) + 1
     ) 
 end
 
@@ -34,5 +41,14 @@ puts "finished with skills"
        thumb_image: "http://placehold.it/#{portfolio_item * 2 + 10}x#{portfolio_item * 2 + 10}"
        ) 
 end
+
+Portfolio.create!(
+    title: "Custom Portfolio",
+    subtitle: "Angular",
+    body: "Amazing text",
+    main_image:  "http://placehold.it/500x750",
+    thumb_image:  "http://placehold.it/50x75"
+    )
+
 
 puts "finished with portfolios"
