53b360f6473875436f09cfc400685e376dca822d
Yrish <Yrish3@gmail.com>
2017-06-21 15:42:16 +0000

    setting data defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,13 @@ class Portfolio < ApplicationRecord
     def self.angular
         where(subtitle: 'Angular')
     end
+    
+    scope :someting_awesome, -> {where(subtitle: "Something Awesome")}
+    
+    after_initialize :set_defaults
+    
+    def set_defaults
+        self.main_image ||= "http://placehold.it/#{Portfolio.all.count * 20 + 10}x#{Portfolio.all.count * 10 + 10}"
+        self.thumb_image ||= "http://placehold.it/#{Portfolio.all.count * 2 + 10}x#{Portfolio.all.count + 10}"
+    end
 end
