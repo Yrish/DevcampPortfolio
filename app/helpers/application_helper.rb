ee82fc80979d8e7b844d3aaa83812696b3e2120e
Yrish <Yrish3@gmail.com>
2017-07-06 17:21:50 +0000

    fixed the login helper so that the blog and profile page are still accesable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,26 @@
 module ApplicationHelper
-    def login_helper(clas)
+    def login_helper(clas = "")
         if (current_user.is_a?(User) && !current_user.is_a?(GuestUser))
             link_to "Logout", destroy_user_session_path, method: :delete, class: clas
         else
             (link_to "Login", new_user_session_path, class: clas) + "\n".html_safe +
             (link_to "Register", new_user_registration_path, class: clas)
+        end
+    end
+    
+    def create_button(text, destination, clas)
+       return "<a href='#{destination}' class='#{clas}'>#{text}</a>".html_safe 
+    end
+    
+    def devise_buttons(key, text, clas)
+        if key == 'login'
+            return create_button text, new_session_path(resource_name), clas
+        end
+        if key == 'sign_up'
+           return create_button text, new_registration_path(resource_name), clas 
+        end
+        if key == 'forgot'
+            return create_button text, new_password_path(resource_name), clas
         end
     end
     
