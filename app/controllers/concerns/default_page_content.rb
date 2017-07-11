ee72b2e98b952ad1cd622a7f517c27baec1a64a7
Yrish <Yrish3@gmail.com>
2017-07-11 15:23:13 +0000

    Working blog

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -14,5 +14,7 @@ module DefaultPageContent
        @instagram = nil_path
        @github = nil_path
        @blogabout = "This is a blog that has been created by #{@name} for resons left undeclared in this variable.  Please fix this by going to 'app/controllers/concerns/default_page_content.rb' and then edit the @blogabout varaible"
+       @portfolioAboutDrop = "This is a drop down in the portfolio page, the default content has not been over writen, please do so before publishing this website"
+       @portfolioAlbumCorrolation = "This is visible on the portfolio index page, it contains information about what the portfolios have in common. Please over ride this before publishing"
     end
 end
