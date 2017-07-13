d2988cde1e6d130ec7c3dfdfb40a7777241a8902
Yrish <Yrish3@gmail.com>
2017-07-13 15:53:14 +0000

    commited file uploading system for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -69,7 +69,7 @@ class PortfoliosController < ApplicationController
     #These are methods for common code
     private
         def portfolio_params
-            params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+            params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, technologies_attributes: [:name])
         end
         
         def set_portfolio
